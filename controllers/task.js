5257f27051785d120ee86d13a355f71f69e5ae52
Sudip Shrestha <xthazudp@gmail.com>
2023-02-03 20:14:55 +0545

    basic route and controller setup

--- controllers/task.js
+++ controllers/task.js
@@ -2,6 +2,26 @@ const getAllTasks = (req, res) => {
   res.send('all items from the file');
 };
 
+const createTask = (req, res) => {
+  res.send('create a single task.');
+};
+
+const getTask = (req, res) => {
+  res.send('get a single task.');
+};
+
+const updateTask = (req, res) => {
+  res.send('update a task.');
+};
+
+const deleteTask = (req, res) => {
+  res.send('delete a task.');
+};
+
 module.exports = {
   getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
 };
--- routes/task.js
+++ routes/task.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllTasks } = require('../controllers/task');
+const {
+  getAllTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
+} = require('../controllers/task');
 
-router.route('/').get(getAllTasks);
+router.route('/').get(getAllTasks).post(createTask);
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
 
 module.exports = router;
