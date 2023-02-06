ad639b934a45f39f168307820b583affbf09e484
Sudip Shrestha <xthazudp@gmail.com>
2023-02-06 18:39:23 +0545

    mongoose setup

--- db/connect.js
+++ db/connect.js
@@ -1,1 +1,14 @@
-const connectionString = mongodb+srv://xthazudp:<password>@nodetaskmanager.wulmn90.mongodb.net/?retryWrites=true&w=majority
+const mongoose = require('mongoose');
+
+const connectionString =
+  'mongodb+srv://xthazudp:1234@nodetaskmanager.wulmn90.mongodb.net/TaskManager?retryWrites=true&w=majority';
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('CONNECTED TO THE DB.'))
+  .catch((err) => console.log(err));
