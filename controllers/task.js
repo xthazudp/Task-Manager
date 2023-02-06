8fbff7885e8a2f8e1415296c68b7af86ed2249f9
Sudip Shrestha <xthazudp@gmail.com>
2023-02-06 20:46:12 +0545

    deleteTask

--- controllers/task.js
+++ controllers/task.js
@@ -26,17 +26,25 @@ const getTask = async (req, res) => {
       return res.status(404).json({ mag: `No task with id : ${taskID}` });
     }
     res.status(200).json({ task });
-  } catch (error) {
-    res.status(500).json({ msg: error });
-  }
+  } catch (error) {}
 };
 
 const updateTask = (req, res) => {
   res.send('update a task.');
 };
 
-const deleteTask = (req, res) => {
-  res.send('delete a task.');
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ mag: `No task with id : ${taskID}` });
+    }
+    res.status(200).json({ task });
+    // res.status(200).send();
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
