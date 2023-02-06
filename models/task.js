028faa9e01ee71daf5dc1079803fde063e827b49
Sudip Shrestha <xthazudp@gmail.com>
2023-02-06 19:43:05 +0545

    adding valiadation

--- models/task.js
+++ models/task.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, 'Must provide name.'],
+    trim: true,
+    maxlength: [20, 'Name can not be more than 20 characters.'],
+  },
+  completed: { type: Boolean, default: false },
 });
 
 module.exports = mongoose.model('Task', TaskSchema);
